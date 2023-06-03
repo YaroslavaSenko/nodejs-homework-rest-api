75411a19f9f7dd38f7d79c7592b57185b0bca876
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-03 14:48:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 
 const mongoose = require('mongoose')
-const DB_HOST = "mongodb+srv://Yaroslava:351086Zz@cluster0.doycfh4.mongodb.net/db-contacts?retryWrites=true&w=majority"
+const {DB_HOST} = process.env;
 
 mongoose.connect(DB_HOST)
 .then(() => {
