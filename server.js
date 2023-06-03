79f099b4b8ebde19818469ae297c70454f1248f5
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-03 14:32:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const mongoose = require('mongoose')
+const DB_HOST = "mongodb+srv://Yaroslava:351086Zz@cluster0.doycfh4.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(3000)
+  console.log("Database connection successful")})
+.catch(error => {
+  console.log(error.message)
+  process.exit(1)
 })
