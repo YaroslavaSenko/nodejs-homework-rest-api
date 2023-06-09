1d1d6722d3bb113a57dbcc9b26dffabab6f427e7
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-09 13:42:59 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 router.post('/register', validateBody(schemas.registerSchema), ctrl.register)
-
+router.post('/login', validateBody(schemas.loginSchema), ctrl.login)
 
 module.exports = router;
 
