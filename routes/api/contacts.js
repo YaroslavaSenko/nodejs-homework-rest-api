74006d6f937c20bd06c304c7b7a2b4b1158320c1
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-09 20:30:20 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,10 +3,7 @@ const express = require('express')
 const contactsController = require('../../controllers/contacts-controller')
 const schemas = require('../../schemas/contactsSchemas')
 
-const validateBody = require("../../middlewares/validateBody")
-const isValidId = require("../../middlewares/isValidId")
-
-const validateFavourite = require('../../middlewares/validateFavourite')
+const {isValidId, validateBody, validateFavourite} = require("../../middlewares")
 
 const router = express.Router();
 
@@ -14,12 +11,11 @@ router.get('/', contactsController.getAllContacts)
 
 router.get('/:id', isValidId, contactsController.getContactById)
 
-
 router.post('/', validateBody(schemas.contactAddSchema), contactsController.addContact)
 
 router.put('/:id', isValidId, validateBody(schemas.contactAddSchema), contactsController.updateContactById )
 
-router.patch("/:id/favorite", isValidId, validateFavourite(schemas.contactUpdateFavoriteSchema), contactsController.updateStatusContact)
+router.patch("/:id/favorite", isValidId, validateFavourite(schemas.updateFavouriteSchema), contactsController.updateStatusContact)
 
 
 router.delete('/:id', isValidId, contactsController.deleteContactById )
