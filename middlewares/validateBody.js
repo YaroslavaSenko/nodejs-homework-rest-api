0c9e6f3af6bec7e4fd58a1b1bd2cdec4c543f506
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-09 20:30:10 +0300

    Update validateBody.js

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,8 +1,6 @@
+const { HttpError } = require("../helpers");
 
-
-const {HttpError} = require('../helpers')
-
-const validateBody = (schema) => {
+const validateBody = schema => {
   const func = (req, res, next) => {
     if (Object.keys(req.body).length === 0) {
       next(HttpError(400, "Missing fields"));
@@ -10,7 +8,7 @@ const validateBody = (schema) => {
     const { error } = schema.validate(req.body);
     if (error) {
       next(
-        HttpError(400, `missing required ${error.details[0].context.key} field`)
+        HttpError(400, `missing field`)
       );
     }
     next();
@@ -19,3 +17,5 @@ const validateBody = (schema) => {
 };
 
 module.exports = validateBody;
+
+
