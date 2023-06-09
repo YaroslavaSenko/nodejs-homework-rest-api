f1ae74676d1dc0ec456469ec4ae516ad2c278b67
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-09 13:42:17 +0300

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,10 +1,18 @@
-const { ctrlWrapper } = require("../decorators")
+const { ctrlWrapper } = require("../decorators");
+const { HttpError } = require("../helpers");
 const { User } = require("../models/user")
-
+const bcrypt = require("bcryptjs")
 
 
 const register = async(req, res) => {
-const newUser = await User.create(req.body);
+    const {email, password} = req.body;
+    const user = await User.findOne({email})
+    if(user){
+        throw HttpError(409, "Email in use")
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10)
+const newUser = await User.create({...req.body, password: hashPassword });
 
 res.status(201).json({
     email: newUser.email,
@@ -13,6 +21,29 @@ res.status(201).json({
 }
 
 
+const login = async(req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email})
+    if(!user){
+        throw HttpError(401, "Email or password is wrong")
+    }
+
+const passwordCompare = await bcrypt.compare(password, user.password);
+if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong")
+}
+const token = "exampletoken"
+
+
+res.status(200).json({
+    token,
+    
+  })
+}
+
+
+
 module.exports = {
     register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
 }
