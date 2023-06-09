890a5f5e55a1ebc938f202452d05b8df9725675f
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-09 14:17:06 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,8 +3,8 @@ const express = require('express')
 const contactsController = require('../../controllers/contacts-controller')
 const schemas = require('../../schemas/contactsSchemas')
 
-const {validateBody} = require("../../decorators")
-const {isValidId} = require("../../middlewares")
+const validateBody = require("../../middlewares/validateBody")
+const isValidId = require("../../middlewares/isValidId")
 
 const validateFavourite = require('../../middlewares/validateFavourite')
 
@@ -14,11 +14,13 @@ router.get('/', contactsController.getAllContacts)
 
 router.get('/:id', isValidId, contactsController.getContactById)
 
+
 router.post('/', validateBody(schemas.contactAddSchema), contactsController.addContact)
 
 router.put('/:id', isValidId, validateBody(schemas.contactAddSchema), contactsController.updateContactById )
 
 router.patch("/:id/favorite", isValidId, validateFavourite(schemas.contactUpdateFavoriteSchema), contactsController.updateStatusContact)
+
 
 router.delete('/:id', isValidId, contactsController.deleteContactById )
 
