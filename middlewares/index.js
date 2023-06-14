57114e2fce1efe320e4c8795ee103c492a680a20
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-14 11:22:19 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,15 @@
 
 const isValidId = require("./isValidId")
 const authorization = require("./authorization")
+const validateBody = require("./validateBody")
+const validateFavourite = require('./validateFavourite')
+const handleMongooseError = require('./handleMongooseError')
 
 
 module.exports = {
-    
+    validateBody,
     isValidId,
+    validateFavourite,
+    handleMongooseError,
     authorization,
 }
