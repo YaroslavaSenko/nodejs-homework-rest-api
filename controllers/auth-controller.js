16eb2f26020782e08e1254079910a730760bfe14
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-14 11:21:36 +0300

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -17,39 +17,62 @@ const register = async(req, res) => {
 const newUser = await User.create({...req.body, password: hashPassword });
 
 res.status(201).json({
-    email: newUser.email,
-    password: newUser.password,
+    user: {
+        email: newUser.email,
+        subscription: newUser.subscription,
+      }
 })
 }
 
 
-const login = async(req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email})
-    if(!user){
-        throw HttpError(401, "Email or password is wrong")
+const login = async (req, res) => {
+    const { email, password} = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw HttpError(401, "Email or password is wrong");
     }
-
-const passwordCompare = await bcrypt.compare(password, user.password);
-if (!passwordCompare) {
-    throw HttpError(401, "Email or password is wrong")
-}
-
-const payload = {
-    id: user._id,
-}
-const token = jwt.sign(payload,SECRET_KEY, {expiresIn: "23h"})
-
-
-res.status(200).json({
-    token,
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw HttpError(401, "Email or password is wrong");
+    }
     
-  })
-}
-
-
-
-module.exports = {
+  
+    const payload = {
+      id: user._id,
+    }
+  
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+     
+    await User.findByIdAndUpdate(user._id, {token})
+    res.json({
+      token, 
+      user: {
+        email,
+        subscription: user.subscription,
+      }  
+    });
+   
+  };
+  
+  const getCurrent = async (req, res) => {
+    const {email, subscription} = req.user;
+    console.log(res.user);
+    res.json({
+      email, 
+      subscription,
+    })
+  }
+  
+  const logout = async (req, res) => {
+    const {_id} = req.user;
+    await User.findByIdAndUpdate(_id, {token: ""});
+    
+    res.status(204).json({});
+  }
+  
+  module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
-}
+    getCurrent: ctrlWrapper(getCurrent),
+    logout: ctrlWrapper(logout),
+  };
