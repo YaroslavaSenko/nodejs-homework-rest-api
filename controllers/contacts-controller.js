598c9d8c321bd3e02708d3b67b95a2fc989f27ff
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-14 10:29:25 +0300

    Update contacts-controller.js

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -6,7 +6,11 @@ const Contact = require('../models/contact')
 
 
 const getAllContacts = async (req, res) => {
-   const result = await Contact.find();
+  const{_id: owner} = req.user;
+  const {page =1, limit = 10} = req.query;
+  const skip = (page - 1) * limit;
+   const result = await Contact.find({owner}, {skip, limit})
+   .populate("owner");
    res.json(result)   
   }
 
@@ -20,7 +24,8 @@ const getAllContacts = async (req, res) => {
   }
 
 const addContact = async (req, res) => {
-const result = await Contact.create(req.body)
+  const{_id: owner} = req.user;
+const result = await Contact.create(...req.body, owner)
 res.status(201).json(result)
 }
 
