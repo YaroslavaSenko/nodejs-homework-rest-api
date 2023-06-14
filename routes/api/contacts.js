1a4ea044c10086ee8694ecb933b93cfd9b9d7a1f
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-14 11:22:45 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,9 +3,7 @@ const express = require('express')
 const contactsController = require('../../controllers/contacts-controller')
 const schemas = require('../../schemas/contactsSchemas')
 
-const {validateBody} = require("../../decorators")
-const {isValidId, authorization } = require("../../middlewares")
-const { validateReqBody } = require('../../helpers')
+const {isValidId, authorization, validateBody, validateFavourite} = require("../../middlewares")
 
 const router = express.Router();
 
@@ -17,9 +15,9 @@ router.get('/:id', isValidId, contactsController.getContactById)
 
 router.post('/', validateBody(schemas.contactAddSchema), contactsController.addContact)
 
-router.put('/:id', isValidId, validateReqBody, validateBody(schemas.contactAddSchema), contactsController.updateContactById )
+router.put('/:id', isValidId, validateBody(schemas.contactAddSchema), contactsController.updateContactById )
 
-router.patch("/:id/favorite", isValidId, validateReqBody, validateBody(schemas.contactUpdateFavoriteSchema), contactsController.updateStatusContact)
+router.patch("/:id/favorite", isValidId, validateFavourite(schemas.contactUpdateFavoriteSchema), contactsController.updateStatusContact)
 
 router.delete('/:id', isValidId, contactsController.deleteContactById )
 
