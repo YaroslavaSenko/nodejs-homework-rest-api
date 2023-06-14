636d94a5b8def3987260f3424ccb3b032426e33f
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-14 11:21:48 +0300

    Delete handleMongooseError.js

--- helpers/handleMongooseError.js
+++
@@ -1,8 +0,0 @@
- const handleMongooseError = (error, data, next) => {
-  const {code, name} = error;
-  error.status =(code === 11000 && name === "MongoServerError") ? 409 : 400;
-    error.status = 400;
-    next()
-  }
-
-  module.exports = handleMongooseError;
