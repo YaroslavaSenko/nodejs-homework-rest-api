cdbb7ad8eb0cf6d40c690c5b4b59171bd80c2972
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-06-29 22:42:17 +0300

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,11 @@
 const HttpError = require('./HttpError')
 const validateReqBody = require('./validateReqBody')
 const handleMongooseError = require('../middlewares/handleMongooseError')
-
+const sendEmail = require("./sendEmail");
 
 module.exports = {
     HttpError,
     validateReqBody,
     handleMongooseError,
+    sendEmail,
 }
